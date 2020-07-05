5574880d6266a6e83aa1e236b6ed307d444dc769
varunmohan05 <varunmohan05@gmail.com>
2020-07-06 00:40:10 +0530

    minor date format change

--- lib/models/message.dart
+++ lib/models/message.dart
@@ -2,7 +2,7 @@ class Message {
   int id;
   int isMe;
   String msg = 'This is a sample message';
-  String time = '${DateTime.now().hour}:${DateTime.now().minute>9?DateTime.now().minute:'0''${DateTime.now().minute}'}';
+  String time = '${DateTime.now().hour>9?DateTime.now().hour:'0''${DateTime.now().hour}'}:${DateTime.now().minute>9?DateTime.now().minute:'0''${DateTime.now().minute}'}';
 
   messageMap() {
     var mapping = Map<String, dynamic>();
